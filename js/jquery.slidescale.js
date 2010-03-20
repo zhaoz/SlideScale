adb2d62667a400f4c1681b47838dffaa4a3282d6
Ziling Zhao <zilingzhao@gmail.com>
2010-03-19 23:13:01 -0700

    basic framework for plugins

--- js/jquery.slidescale.js
+++ js/jquery.slidescale.js
@@ -1,2 +1,25 @@
 (function ($) {
+$.fn.slidescale = function (options) {
+    this.each(function () {
+        var elem = $(this),
+            obj = $.slidescale(elem, options);
+        elem.data('slidescale', obj);
+    });
+
+    return this;
+};
+
+$.slidescale = function (container, options) {
+    this.container = container;
+
+    this.opts = $.extend({}, $.slidescale.defaults, options);
+ 
+};
+
+$.slidescale.defaults = {
+};
+
+$.slidescale.prototype = {
+};
+
 }(jQuery));
