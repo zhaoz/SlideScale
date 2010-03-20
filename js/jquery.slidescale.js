5233ce81f309f2461ee52fa020b5e6571306bd7e
Ziling Zhao <zilingzhao@gmail.com>
2010-03-19 23:57:48 -0700

    Some skeleton functions for adding images
     * taking images already in list and adding to internal list
     * taking images passed in and adding to list

--- js/jquery.slidescale.js
+++ js/jquery.slidescale.js
@@ -10,17 +10,60 @@ $.fn.slidescale = function (options) {
 };
 
 $.slidescale = function (container, options) {
+    var ii, imgs, o;
+
     this.container = container;
-    this.opts = $.extend({}, $.slidescale.defaults, options);
+    o = this.opts = $.extend({}, $.slidescale.defaults, options);
 
     this.container.addClass('slidescale')
         .children('ol').addClass('slidescale-list');
+
+    this.images = [];
+
+    this._initImages();
+
+    if (o.images) {
+        imgs = o.images;
+        for (ii = 0; ii < imgs.length; ii++) {
+            this.addImage(imgs[ii]);
+        }
+    }
+
+    this.curImage = 0;
+    this.setImageIndex(o.startingIndex);
+
 };
 
 $.slidescale.defaults = {
+    startingIndex: 0                // index of images to start on 
 };
 
 $.slidescale.prototype = {
+
+_initImages: function () {
+    var that = this;
+    this.container.find('ol.slidescale-list > li').each(function () {
+        var el = $(this);
+        that.addImage({ url: el.find('img').attr('src'),
+            text: el.find('caption').text() });
+    });
+},
+
+/**
+ * Take an image hash, img.url, img.text, add to images list
+ */
+addImage: function (img) {
+    this.images.push(img);
+
+    // TODO need to check index and load if needed
+},
+
+setImageIndex: function (ii) {
+    this.curImage = Math.max(Math.min(this.images.length - 1, ii || 0), 0);
+
+    // TODO all types of crazy effects
+}
+
 };
 
 }(jQuery));
